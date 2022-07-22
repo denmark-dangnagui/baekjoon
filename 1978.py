9e7526dedc189196511c7b819739c57622d77e56
denmark-dangangui <dlrgks88@naver.com>
2022-07-22 13:34:50 +0900

    백준 1978번

---
+++ 1978.py
@@ -0,0 +1,12 @@
+n = int(input())
+numbers = list(map(int,input().split()))
+prime = 0
+for i in numbers:
+    cnt = 0
+    if i > 1:
+        for j in range(2,i):
+            if i % j == 0:
+                cnt += 1
+        if cnt == 0:
+            prime += 1
+print(prime)
