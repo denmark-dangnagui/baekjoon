0f275cf2b74f06ac18ddf012707a58d4b8e75bf9
denmark-dangangui <dlrgks88@naver.com>
2022-07-24 01:27:00 +0900

    백준 4948번(에라토스테네스의 체)

---
+++ 4948.py
@@ -0,0 +1,18 @@
+def isPrime(n2,n):
+    a = [True] * (n2 + 1)
+    m = int(n2**0.5)
+    for i in range(2, m + 1):
+        if a[i] == True:
+            for j in range(i + i, n2 + 1, i):
+                a[j] = False
+    print(len([i for i in range(n+1, n2) if a[i] == True]))
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    elif n == 1:
+        print(1)
+    else:
+        n2 = n * 2
+        isPrime(n2,n)
