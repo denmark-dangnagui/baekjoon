79f090950036fee43d9be3bde31975960f61a10a
denmark-dangangui <dlrgks88@naver.com>
2022-07-29 06:03:46 +0900

    Update 11650.py

--- 11650.py
+++ 11650.py
@@ -5,7 +5,7 @@ for i in range(n):
     nums = list(map(int,sys.stdin.readline().split()))
     numbers.append(nums)
 
-numbers.sort(key = lambda x:-x[0])
+numbers.sort(key = lambda x : x[0])
 for i in range(n):
     for j in range(2):
         print(numbers[i][j],end = ' ')
