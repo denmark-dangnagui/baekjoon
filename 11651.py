e0fed485dfe8fec05801d07b465d3e8d8f255ebe
denmark-dangangui <dlrgks88@naver.com>
2022-07-30 03:21:57 +0900

    정렬시 우선순위를 lambda에 넣어주면 됨

--- 11651.py
+++ 11651.py
@@ -5,7 +5,7 @@ for i in range(n):
     a = list(map(int,sys.stdin.readline().split()))
     numbers.append(a)
 
-numbers.sort(key = lambda x : x[1])
+numbers.sort(key = lambda x : (x[1], x[0]))
 for i in range(n):
     for j in range(2):
         print(numbers[i][j],end=' ')
