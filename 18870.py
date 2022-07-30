1740745b336dc24e892498e99d67206f6d47a6e8
denmark-dangangui <dlrgks88@naver.com>
2022-07-31 01:55:56 +0900

    Update 18870.py

--- 18870.py
+++ 18870.py
@@ -1,11 +1,12 @@
 import sys
 
-n = int(input())
+n = int(sys.stdin.readline())
 
 numbers = list(map(int,sys.stdin.readline().split()))
 
 numbers1 = list(sorted(set(numbers)))
 
 dic = {numbers1[i] : i for i in range(len(numbers1))}
+
 for i in numbers:
-    print(dic[i], end =' ')
+    print(dic[i],end=' ')
