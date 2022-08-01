4a73b5ae2b9c2e261004d6863cb5f358e4c25295
denmark-dangangui <dlrgks88@naver.com>
2022-08-01 16:19:47 +0900

    수정!

---
+++ basic1/9021.py
@@ -0,0 +1,6 @@
+sentence = 'i am happy'
+reverse_sectence = ''
+
+for i in sentence:
+    reverse_sectence = i + reverse_sectence
+print(reverse_sectence)
--- basic1/9030.py
+++ basic1/9030.py
@@ -5,9 +5,5 @@ n = int(sys.stdin.readline())
 for i in range(n):
     s = sys.stdin.readline().split()
     for j in s:
-        j = list(j)
-        j.reverse()
-        a = ''.join(j)
-        print(a, end='')
-        print(end=' ')
+        print(j[::-1],end=' ')
     print()
