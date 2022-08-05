410dc6f264572d7c8e26edbed7d7831f55397c6d
denmark-dangangui <dlrgks88@naver.com>
2022-08-05 16:36:39 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,25 +1,5 @@
-n,m = map(int, input().split())
-chess = []
-check = []
+import numpy as np
 
-for i in range(n):
-    chess.append(input())
-
-for a in range(n - 7):
-    for b in range(m - 7):
-        index1 = 0
-        index2 = 0
-        for i in range(a,a+8):
-            for j in range(b, b + 8):
-                if (i + j) % 2 == 0:
-                    if chess[i][j] != 'W':
-                        index1 += 1
-                    if chess[i][j] != 'B':
-                        index2 += 1
-                else:
-                    if chess[i][j] != 'B':
-                        index1 += 1
-                    if chess[i][j] != 'W':
-                        index2 += 1
-        check.append(min(index1,index2))
-print(min(check))
+test_array = np.array(['1','4',5,6],float)
+print(test_array)
+print(type(test_array[3]))
