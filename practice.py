7421f035082ed804f9b5368b9937d47d32b2339e
denmark-dangangui <dlrgks88@naver.com>
2022-08-08 11:05:17 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,5 +1,17 @@
-import numpy as np
-
-test_array = np.array(['1','4',5,6],float)
-print(test_array)
-print(type(test_array[3]))
+n = int(input())
+movement = list(map(str,input().split()))
+location = [1,1]
+for i in movement:
+    if i == 'R':
+        if location[1] + 1 <= n:
+            location[1] += 1
+    elif i == 'L':
+        if location[1] - 1 > 0:
+            location[1] -= 1
+    elif i == 'U':
+        if location[0] - 1 > 0:
+            location[0] -= 1
+    elif i == 'D':
+        if location[0] + 1 <= n:
+            location[0] += 1
+print(location)
