35bf99baf1c46ab883529594d5a8c563342b9593
denmark-dangangui <dlrgks88@naver.com>
2022-09-26 01:07:16 +0900

    two stack use

--- basic1/9012.py
+++ basic1/9012.py
@@ -1,28 +1,34 @@
 import sys
 
+stack1 = []
+stack2 = []
+size1 = 0
 n = int(sys.stdin.readline())
 
-for k in range(n):
-    s = sys.stdin.readline()
-    size = 0
-    stack = []
-    temp = []
-    for i in range(len(s) - 1):
-        stack.append(s[i])
-    for i in range(len(s) - 1):
-        if stack[-1] == ')':
-            temp.append(stack[-1])
-            stack.pop()
-        elif stack[-1] == '(':
-            if len(temp) == 0:
-                temp.append(stack[-1])
-            elif temp[-1] == ')':
-                temp.pop()
-                stack.pop()
+for i in range(n):
+    g = sys.stdin.readline()
+    for j in g:
+        stack1.append(j)
+    stack1.remove('\n')
+    for j in range(len(stack1)):
+        temp = stack1.pop()
+        if len(stack2) == 0:
+            if temp == '(':
+                stack2.append(temp)
+                break
             else:
-                temp.append(stack[-1])
-                stack.pop()
-    if len(temp) != 0:
-        print('NO')
-    else:
+                stack2.append(temp)
+        else:
+            if temp == '(':
+                if stack2[-1] == ')':
+                    stack2.pop()
+                else:
+                    stack2.append(temp)
+            else:
+                stack2.append(temp)
+    if len(stack2) == 0:
         print('YES')
+    else:
+        print('NO')
+    stack2.clear()
+    stack1.clear()
