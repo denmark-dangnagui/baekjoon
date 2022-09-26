5f27fe63f5c9e53ec6cf30aa2f52d33050fdfb3f
denmark-dangangui <dlrgks88@naver.com>
2022-09-26 14:02:53 +0900

    stack lifo 활용

--- basic1/1874.py
+++ basic1/1874.py
@@ -1,25 +1,23 @@
 import sys
 n = int(sys.stdin.readline())
-temp = []
+j = 0
+answer = []
 stack1 = []
 stack2 = []
 stack3 = []
 for i in range(n):
-    temp.append(int(sys.stdin.readline()))
-max_num = 0
-for j in temp:
-    if j > max_num:
-        for i in range(max_num + 1,j + 1):
-            stack1.append(i)
-            stack2.append('+')
-        max_num = j
-        stack2.append('-')
-        stack3.append(stack1.pop())
-    else:
-        stack3.append(stack1.pop())
-        stack2.append('-')
-if temp == stack3:
-    for i in stack2:
+    stack1.append(int(sys.stdin.readline()))
+for i in range(1,n + 1):
+    cnt = 0
+    stack2.append(i)
+    answer.append('+')
+    while stack2 and stack1[j] == stack2[-1]:
+        stack3.append(stack2.pop())
+        j += 1
+        cnt += 1
+        answer.append('-')
+if stack1 == stack3:
+    for i in answer:
         print(i)
 else:
-    print("NO")
+    print('NO')
