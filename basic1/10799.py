8ad2705a1e6b3fae30e463b4f35524cf4fd690a7
denmark-dangangui <dlrgks88@naver.com>
2022-10-01 23:21:32 +0900

    Update 10799.py

--- basic1/10799.py
+++ basic1/10799.py
@@ -3,13 +3,20 @@ stack = []
 stick = list(sys.stdin.readline())
 stick.remove('\n')
 answer = 0
-cnt = 0
+temp = 0
 for i in stick[::-1]:
-    if i == ')':
-        stack.append(stick.pop())
-    if i == '(':
-        stack.pop()
-        answer += len(stack)
-
-    temp = i
-# print(stick)
+    if temp == '(':
+        if i == '(':
+            stack.pop()
+            answer += 1
+        else:
+            stack.append(stick.pop())
+        temp = i
+    else:
+        if i == ')':
+            stack.append(stick.pop())
+        if i == '(':
+            stack.pop()
+            answer += len(stack)
+        temp = i
+print(answer)
